d884c06f2c57294d10ec41cff8a71c0400ee3ebe
liuyoshio <liuyoshio2001@gmail.com>
2023-01-22 21:35:54 +0900

    trivial

--- utils/config.py
+++ utils/config.py
@@ -6,7 +6,7 @@ HR_SIZE = 256
 LR_SIZE = 128
 
 # Path to the WorldStrat dataset (High Resolution)
-WorldStrat_path = "/Users/terry/Desktop/WorldStrat_hr/"
+WorldStrat_path = "/Users/terry/Desktop/WorldStrat_hr"
 
 transform_hr = transforms.Compose([
     transforms.ToTensor(),
