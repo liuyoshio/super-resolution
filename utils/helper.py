e48eb663a80367af4d2b03eef525b14377de4a04
liuyoshio <liuyoshio2001@gmail.com>
2023-02-04 15:16:03 +0900

    add calculate psnr and ssim

--- utils/helper.py
+++ utils/helper.py
@@ -1,5 +1,8 @@
+import os
 import numpy as np
-import matplotlib.pyplot as plt
+import torch
+import matplotlib.pyplot as plt 
+from .config import DEVICE
 
 # Visualize the dataset
 # example: datasets[0] -> (HR image, LR image)
@@ -23,3 +26,10 @@ def random_crop(img, crop_size):
     left = np.random.randint(0, w - crop_size)
     img = img[top:top+crop_size, left:left+crop_size]
     return img
+
+# laod model dict
+def load_model_dict(model, model_path):
+    if not os.path.isfile(model_path):
+        print ('Model path is not exist!')
+        return
+    model.load_state_dict(torch.load('model_path', map_location=DEVICE))
